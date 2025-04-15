31ef36780c393519c06f04951c574114afc4c24b
x23167645 <x23167645@student.ncirl.ie>
2025-04-15 02:41:09 +0100

    v2

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Dockerized Flask app on Azure!"
+    return "Hello from Dockerized Flask app on Azure!- V2"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
